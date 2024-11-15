790e9f6cb533d303bbc2402fb5c12a4cbc2cb860
Namikazes <egorsitenko35@gmail.com>
2024-11-15 22:07:57 +0200

    ChatList

---
+++ src/components/Login/blocks/Logout.tsx
@@ -0,0 +1,20 @@
+const Logout = () => {
+  const isAuthenticated = localStorage.getItem('token'); 
+
+  const logout = () => {
+    localStorage.clear();
+    window.location.href = '/login';
+  }
+
+  return (
+    <div>
+      {isAuthenticated && (
+        <button onClick={logout}>
+          LOGOUT
+        </button>
+      )}
+    </div>
+  );
+}
+
+export default Logout;
